90703a8f25ad8bb5d2b68dd39fa907c11bf07866
Your Name <vinhmai2099@gmail.com>
2023-12-18 20:16:57 +0700

    Move pieces

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -7,10 +7,18 @@ interface Props{
 export default function Tile({number, image} : Props) {
     if(number % 2 === 0)
     {
-        return <div className="tile black-tile"><img src={image}/></div>
+        return (<div className="tile black-tile">
+             {image && <div style={{backgroundImage: `url(${image})`}} className="chess-piece"> </div>}
+        </div>
+        );
+           
+
     }
     else{
-        return <div className="tile white-tile"><img src={image}/></div>
+        return (<div className="tile white-tile">
+                    {image && <div style={{backgroundImage: `url(${image})`}} className="chess-piece"> </div>}
+                </div>
+        );
     }
     
 }
