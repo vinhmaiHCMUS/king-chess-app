6a050eec9d214bddb690db44c8264336175eadf2
Your Name <vinhmai2099@gmail.com>
2023-12-18 15:32:14 +0700

    Create background color of Chessboard

--- src/components/ChessBoard.tsx
+++ src/components/ChessBoard.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import './ChessBoard.css';
 
-const verticalAxis = [1, 2, 3, 4, 5, 6, 7, 8];
+const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
 const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
 export default function ChessBoard(){
-    return <div id="chessboard"></div>
+    let board = [];
+    for(let j = verticalAxis.length - 1; j >= 0; j--)
+    {
+        for(let i = 0; i < horizontalAxis.length; i++)
+        {
+            const number = j + i + 2;
+            if(number %2 === 0)
+            {
+                board.push(<div className="tile black-tile"></div>)
+            }
+            else
+            {
+                board.push(<div className="tile white-tile"></div>)
+            }
+            
+        }
+    }
+   
+    return <div id="chessboard">{board}</div>
 }
