fadb5ba5335c6835e7aa14a8f91feac14f1f7676
Your Name <vinhmai2099@gmail.com>
2023-12-18 14:35:15 +0700

    Add assests,components

---
+++ src/components/ChessBoard.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './ChessBoard.css';
+
+const verticalAxis = [1, 2, 3, 4, 5, 6, 7, 8];
+const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
+export default function ChessBoard(){
+    return <div id="chessboard"></div>
+}
