96d1f76a2855c870637226029c94136f3fad5d89
Your Name <vinhmai2099@gmail.com>
2023-12-18 16:23:55 +0700

    Add pawn pieces to the chessboard

--- src/components/ChessBoard/ChessBoard.tsx
+++ src/components/ChessBoard/ChessBoard.tsx
@@ -4,6 +4,24 @@ import './ChessBoard.css';
 const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
 const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
+interface Piece{
+    image: string;
+    x: number;
+    y: number;
+}
+
+const pieces: Piece[] = [];
+
+for(let i = 0; i < 8; i++)
+{
+    pieces.push({image: "assets/images/pawn_b.png", x: i, y: 6})
+}
+
+for(let i = 0; i < 8; i++)
+{
+    pieces.push({image: "assets/images/pawn_w.png", x: i, y: 1})
+}
+
 export default function ChessBoard(){
     let board = [];
     for(let j = verticalAxis.length - 1; j >= 0; j--)
@@ -11,7 +29,14 @@ export default function ChessBoard(){
         for(let i = 0; i < horizontalAxis.length; i++)
         {
             const number = j + i + 2;
-            board.push(<Tile number = {number}/>)
+            let image = undefined;
+            pieces.forEach(p => {
+                if(p.x === i && p.y === j)
+                {
+                    image = p.image;
+                }
+            })
+            board.push(<Tile image = {image} number = {number}/>)
         }
     }
    
--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -1,15 +1,16 @@
 import './Tile.css';
 
 interface Props{
+    image?: string;
     number: number;
 }
-export default function Tile({number} : Props) {
+export default function Tile({number, image} : Props) {
     if(number % 2 === 0)
     {
-        return <div className="tile black-tile"></div>
+        return <div className="tile black-tile"><img src={image}/></div>
     }
     else{
-        return <div className="tile white-tile"></div>
+        return <div className="tile white-tile"><img src={image}/></div>
     }
     
 }
