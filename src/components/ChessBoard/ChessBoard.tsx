f6dae20d248526781a17738d056aebbd8d8aa690
Your Name <vinhmai2099@gmail.com>
2023-12-19 19:01:16 +0700

    Tao luat an quan cho quan tot

--- src/components/ChessBoard/ChessBoard.tsx
+++ src/components/ChessBoard/ChessBoard.tsx
@@ -120,27 +120,46 @@ export default function ChessBoard(){
             const x= Math.floor((e.clientX - chessboard.offsetLeft) / 100);
             const y = Math.abs(Math.ceil((e.clientY - chessboard.offsetTop - 800) / 100));
 
+            const currentPiece = pieces.find(p => p.x === gridX && p.y === gridY);
+            const attackedPiece = pieces.find(p => p.x === x && p.y === y);
 
-
-            //Cập nhật vị trí quân cờ
-            setPieces((value) =>{
-                const pieces = value.map(p=>{
-                    if(p.x === gridX && p.y === gridY){
-                        const validMove = referee.isValidMove(gridX, gridY, x, y, p.type, p.team, value);
-                        if(validMove){
-                            p.x = x;
-                            p.y = y;
-                        }else {
-                            activePiece.style.position = 'relative';
-                            activePiece.style.removeProperty('top');
-                            activePiece.style.removeProperty('left');
+            if(currentPiece){
+                const validMove = referee.isValidMove(gridX, gridY, x, y, currentPiece.type, currentPiece.team, pieces);
+                if(validMove){
+                    //Cập nhật vị trí quân cờ
+                    const updatedPieces = pieces.reduce((results, piece) => {
+                        if(piece.x === currentPiece.x && piece.y === currentPiece.y){
+                            piece.x = x;
+                            piece.y = y;
+                            results.push(piece);
+                        } else if(!(piece.x === x && piece.y === y)){
+                            results.push(piece);
                         }
-                        
-                    }
-                    return p;
-                });
-                return pieces;
-            });
+                        return results;
+                    }, [] as Piece[]);
+
+                    setPieces(updatedPieces);
+                    // setPieces((value)=>{
+                    //     const pieces = value.reduce((results, piece) => {
+                    //         if(piece.x === currentPiece.x && piece.y === currentPiece.y){
+                    //             piece.x = x;
+                    //             piece.y = y;
+                    //             results.push(piece);
+                    //         } else if(!(piece.x === x && piece.y === y)){
+                    //             results.push(piece);
+                    //         }
+                    //         return results;
+                    //     }, [] as Piece[]);
+    
+                    //     return pieces;
+                    // })
+                } else{
+                    //Trả quân cờ về vị trí cũ
+                    activePiece.style.position = 'relative';
+                    activePiece.style.removeProperty('top');
+                    activePiece.style.removeProperty('left');
+                }
+            }
             setActivePiece(null);
         }
     }
--- src/referee/Referee.ts
+++ src/referee/Referee.ts
@@ -10,17 +10,38 @@ export default class Referee{
             return false;
         }
     }
+    TileIsOccupiedByOpponent(x: number, y: number, boardState: Piece[], team: TeamType): boolean{
+        const piece = boardState.find((p) => p.x === x && p.y === y && p.team !== team);
+        if(piece){
+            return true;
+        } else{
+            return false;
+        }
+    }
     isValidMove(px: number, py: number, x: number, y: number, type: PieceType, team: TeamType, boardState: Piece[]) {
         if(type === PieceType.PAWN){
             const specialRow = (team === TeamType.NGUOICHOI) ? 1 : 6;
             const pawnDirection = (team === TeamType.NGUOICHOI) ? 1 : -1;
 
+            //Loigc về sự di chuyển của quân tốt
             if(px === x && py === specialRow && y - py === 2*pawnDirection){
                 if(!this.tileIsOccupied(x, y, boardState) && !this.tileIsOccupied(x, y - pawnDirection, boardState)){
                     return true;
                 }
             } else if(px === x && y - py === pawnDirection){
                 if(!this.tileIsOccupied(x, y, boardState)){
+                    return true;
+                }
+            }
+            //Logic về ăn quân của quân tốt
+            else if(x - px === -1 && y - py === pawnDirection){
+                //Tấn công vào phía trên hoặc góc dưới trái
+                if(this.TileIsOccupiedByOpponent(x, y, boardState, team)){
+                    return true;
+                }
+            }else if(x - px === 1 && y - py === pawnDirection){
+                //Tấn công phía trên phải hoặc góc dưới phải
+                if(this.TileIsOccupiedByOpponent(x, y, boardState, team)){
                     return true;
                 }
             }
