142f278829101442f38131aa5166c74ffc12589a
Your Name <vinhmai2099@gmail.com>
2023-12-19 18:04:26 +0700

    Tao luat co ban cho quan pawn

--- src/referee/Referee.ts
+++ src/referee/Referee.ts
@@ -1,22 +1,31 @@
-import { PieceType, TeamType } from "../components/ChessBoard/ChessBoard";
+import { PieceType, TeamType, Piece } from "../components/ChessBoard/ChessBoard";
 
 export default class Referee{
-    isValidMove(px: number, py: number, x: number, y: number, type: PieceType, team: TeamType) {
-        if (type === PieceType.PAWN && team === TeamType.NGUOICHOI) {
-          // Check if the pawn is at the starting position
-          if (py === 1) {
-            // Allow moving one or two squares forward
-            if (px === x && (y - py === 1 || y - py === 2)) {
-              return true;
-            }
-          } else {
-            // Allow moving one square forward
-            if (px === x && y - py === 1) {
-              return true;
-            }
-          }
+    tileIsOccupied(x: number, y:number, boardState: Piece[]): boolean{
+
+        const piece = boardState.find(p => p.x === x && p.y === y)
+        if(piece){
+            return true;
+        }else {
+            return false;
         }
+    }
+    isValidMove(px: number, py: number, x: number, y: number, type: PieceType, team: TeamType, boardState: Piece[]) {
+        if(type === PieceType.PAWN){
+            const specialRow = (team === TeamType.NGUOICHOI) ? 1 : 6;
+            const pawnDirection = (team === TeamType.NGUOICHOI) ? 1 : -1;
+
+            if(px === x && py === specialRow && y - py === 2*pawnDirection){
+                if(!this.tileIsOccupied(x, y, boardState) && !this.tileIsOccupied(x, y - pawnDirection, boardState)){
+                    return true;
+                }
+            } else if(px === x && y - py === pawnDirection){
+                if(!this.tileIsOccupied(x, y, boardState)){
+                    return true;
+                }
+            }
+        }
+
         return false;
-      }
-      
+    }     
 }
